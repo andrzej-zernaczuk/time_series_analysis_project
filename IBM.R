82eb7dde54f87b1da7833a279ee8a31ae04ae38e
Andrzej Żernaczuk <andrzej.zernaczuk@gmail.com>
2023-09-06 16:18:36 +0200

    final

--- IBM.R
+++ IBM.R
@@ -24,13 +24,9 @@ adf.test(IBM_ts)
 kpss.test(IBM_ts)
 
 IBM_diff = diff(diff(IBM_ts))
-
-# Differencing
-par(mfrow=c(1, 1))
-plot(IBM_diff, main="Zróżnicowany szereg", xlab="Lata", ylab="")
-acf(IBM_diff, lag.max = 100, main="Korelacja zróżnicowanego szeregu")
-pacf(IBM_diff, lag.max = 100, main="Cząstkowa korelacja zróżnicowanego szeregu")
-
+tsdisplay(IBM_diff)
+acf(IBM_diff, lag.max = 200, main="Korelacja zróżnicowanego szeregu")
+pacf(IBM_diff, lag.max = 200, main="Cząstkowa zróżnicowanego korelacja szeregu")
 
 # Dickey-Fuller and KPS on differenced data
 adf.test(IBM_diff)
@@ -39,64 +35,48 @@ kpss.test(IBM_diff)
 # Box-Ljung test
 Box.test(IBM_diff, type="Ljung-Box", lag=12)
 
+tsdisplay(IBM_diff)
+acf(IBM_ts, lag.max = 200, main="Korelacja szeregu")
+pacf(IBM_ts, lag.max = 200, main="Cząstkowa korelacja szeregu")
+
 # train/test set
-IBM_train <- window(IBM_diff, end=c(2023, 5))
-IBM_test <- window(IBM_diff, start=c(2023, 6))
+IBM_train <- window(IBM_ts, end=c(2023, 5))
+IBM_test <- window(IBM_ts, start=c(2023, 6))
 
 # horizon for predictions and indicators
 horizon <- length(IBM_test)
 indicators <- c("RMSE", "MAE", "MAPE", "MASE")
 
 
-ARIMA = arima(IBM_train, order=c(1,2,1), include.mean=FALSE)
+ARIMA = Arima(IBM_train, order=c(0,2,0))
 summary(ARIMA)
-BIC(ARIMA)
 
 ARIMA_forecast <- forecast(ARIMA, h=horizon)
 accuracy(ARIMA_forecast, IBM_test)[, indicators]
 plot(ARIMA_forecast, main="Predykcja ARIMA")
 
+ARIMA_output <- data.frame(
+  "Point Forecast" = ARIMA_forecast$mean,
+  "Lo 80" = ARIMA_forecast$lower[, 1],
+  "Hi 80" = ARIMA_forecast$upper[, 1],
+  "Lo 95" = ARIMA_forecast$lower[, 2],
+  "Hi 95" = ARIMA_forecast$upper[, 2]
+)
+ARIMA_output
 
-HOLT = holt(IBM_train)
+
+HOLT = holt(IBM_train, h=3)
 summary(HOLT)
-BIC(HOLT)
 
 HOLT_forecast <- forecast(HOLT, h=horizon)
 accuracy(HOLT_forecast, IBM_test)[, indicators]
 plot(HOLT_forecast, main="Predykcja HOLT")
 
 
-IBM_forecast = cbind(IBM_test, ARIMA_forecast$mean, HOLT_forecast$mean )
-plot.ts(IBM_forecast, plot.type="single", col = c("green", "blue", "red"),
-        ylab = "Cena",
-        xlab = "Czas",
-        main = "Porównanie wartości realnych z predykcjami ARIMA i Holt",
-        lwd = 1.5)
-legend("topright", legend=c("Wartości realne", "ARIMA", "Holt"), col = c("green", "blue", "red"), lwd=2)
-
-plot(window(IBM_ts, start=c(2023, 6)), type="l", col="green", lwd=2, xaxt="n", main="Wyniki predykcji", ylab="", xlab="", ylim=range(c(window(IBM_ts, start=c(2023, 6)), ARIMA_forecast_orig$mean, HOLT_forecast_orig$mean)))
+plot(window(IBM_ts, start=c(2023, 6)), type="l", col="green", lwd=2, xaxt="n", main="Wyniki predykcji", ylab="", xlab="", ylim=range(c(window(IBM_ts, start=c(2023, 6)), ARIMA_forecast$mean, HOLT_forecast$mean)))
 # Add the forecasts to the plot
-lines(ARIMA_forecast_orig$mean, col="blue", lwd=2)
-lines(HOLT_forecast_orig$mean, col="red", lwd=2)
+lines(ARIMA_forecast$mean, col="blue", lwd=2)
+lines(HOLT_forecast$mean, col="red", lwd=2)
 
 # Add a legend
 legend("topright", legend=c("Wartości realne", "ARIMA", "Holt"), col=c("green", "blue", "red"), lwd=2)
-
-
-ARIMA_output <- data.frame(
-  "Point Forecast" = ARIMA_forecast_orig$mean,
-  "Lo 80" = ARIMA_forecast_orig$lower[, 1],
-  "Hi 80" = ARIMA_forecast_orig$upper[, 1],
-  "Lo 95" = ARIMA_forecast_orig$lower[, 2],
-  "Hi 95" = ARIMA_forecast_orig$upper[, 2]
-)
-ARIMA_output
-
-HOLT_output <- data.frame(
-  "Point Forecast" = HOLT_forecast_orig$mean,
-  "Lo 80" = HOLT_forecast_orig$lower[, 1],
-  "Hi 80" = HOLT_forecast_orig$upper[, 1],
-  "Lo 95" = HOLT_forecast_orig$lower[, 2],
-  "Hi 95" = HOLT_forecast_orig$upper[, 2]
-)
-HOLT_output
--- Norway_energy.R
+++ Norway_energy.R
@@ -22,9 +22,78 @@ seasonplot(energy_ts, year.labels=TRUE, col=rainbow(5), main="Dekompozycja sezon
 adf.test(energy_ts)
 kpss.test(energy_ts)
 
+energy_diff <- diff(diff(energy_ts, 12))
+plot(energy_diff, main="Zróżnicowany szereg", xlab="Lata", ylab="")
+tsdisplay(energy_diff)
+acf(energy_diff, lag.max = 200, main="Korelacja zróżnicowanego szeregu")
+pacf(energy_diff, lag.max = 200, main="Cząstkowa zróżnicowanego korelacja szeregu")
+
+# Dickey-Fuller and KPS on differenced data
+adf.test(energy_diff)
+kpss.test(energy_diff)
+
 # Box-Ljung test
-Box.test(energy_ts, type="Ljung-Box", lag=12)
+Box.test(energy_diff, type="Ljung-Box", lag=12)
+tsdisplay(energy_diff)
+acf(energy_ts, lag.max = 200, main="Korelacja szeregu")
+pacf(energy_ts, lag.max = 200, main="Cząstkowa korelacja szeregu")
 
-acf(energy_ts, lag.max = 100, main="Korelacja szeregu")
-pacf(energy_ts, lag.max = 100, main="Cząstkowa korelacja szeregu")
+# train/test set
+energy_train <- window(energy_ts, end=c(2022, 7))
+energy_test <- window(energy_ts, start=c(2022, 8))
 
+# horizon for predictions and indicators
+horizon <- length(energy_test)
+indicators <- c("RMSE", "MAE", "MAPE", "MASE")
+
+
+SARIMA_auto = auto.arima(energy_train, d=2, D=1)
+summary(SARIMA)
+
+SARIMA = Arima(energy_train, order=c(1, 2, 0), seasonal=c(0, 1, 0))
+summary(SARIMA)
+
+SARIMA1 = Arima(energy_train, order=c(2, 2, 0), seasonal=c(0, 1, 0))
+summary(SARIMA1)
+
+SARIMA2 = Arima(energy_train, order=c(2, 2, 1), seasonal=c(0, 1, 0))
+summary(SARIMA2)
+
+
+SARIMA_forecast <- forecast(SARIMA2, h=horizon)
+accuracy(SARIMA_forecast, energy_test)[, indicators]
+plot(SARIMA_forecast, main="Predykcja ARIMA")
+
+SARIMA_output <- data.frame(
+  "Point Forecast" = SARIMA_forecast$mean,
+  "Lo 80" = SARIMA_forecast$lower[, 1],
+  "Hi 80" = SARIMA_forecast$upper[, 1],
+  "Lo 95" = SARIMA_forecast$lower[, 2],
+  "Hi 95" = SARIMA_forecast$upper[, 2]
+)
+SARIMA_output
+
+
+HOLT_add = hw(energy_train, h=12, seasonal="additive")
+summary(HOLT_add)
+
+HOLT_forecast_add <- forecast(HOLT_add, h=horizon)
+accuracy(HOLT_forecast_add, energy_test)[, indicators]
+plot(HOLT_forecast_add, main="Predykcja HOLT")
+
+HOLT_multi = hw(energy_train, h=12, seasonal="multiplicative")
+summary(HOLT_multi)
+
+HOLT_forecast_multi <- forecast(HOLT_multi, h=horizon)
+accuracy(HOLT_forecast_multi, energy_test)[, indicators]
+plot(HOLT_forecast_multi, main="Predykcja HOLT")
+
+
+plot(window(energy_ts, start=c(2022, 8)), type="l", col="green", ylab="Ilość energii", xaxt="n", xlab="", lwd=2, main="Wyniki predykcji", 
+     ylim=range(c(window(energy_ts, start=c(2022, 8)), SARIMA_forecast$mean, HOLT_forecast_multi$mean)))
+# Add the forecasts to the plot
+lines(SARIMA_forecast$mean, col="blue", lwd=2)
+lines(HOLT_forecast_multi$mean, col="red", lwd=2)
+
+# Add a legend
+legend("topright", legend=c("Wartości realne", "SARIMA", "Holt-Winters"), col=c("green", "blue", "red"), lwd=2)
